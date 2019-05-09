539f0dfce495bc3522e82594a79e6288739499e9
alok <alok@unacademy.com>
2019-05-09 19:39:08 +0530

    parent-shutdown-time from env

--- ambassador/kick_ads.sh
+++ ambassador/kick_ads.sh
@@ -24,9 +24,15 @@ if [ -z "$DRAIN_TIME" ]; then
     DRAIN_TIME=600
 fi
 
+if [ -z "$DRAIN_TIME_PARENT" ]; then
+    DRAIN_TIME_PARENT=600
+fi
+
+
+
 if [ -z "$ENVOY_RUNNING" ]; then
     # Envoy isn't running. Start it.
-    envoy $ENVOY_DEBUG -c "${ENVOY_BOOTSTRAP_FILE}" --drain-time-s DRAIN_TIME &
+    envoy $ENVOY_DEBUG -c "${ENVOY_BOOTSTRAP_FILE}" --drain-time-s $DRAIN_TIME --parent-shutdown-time-s $DRAIN_TIME_PARENT &
     ENVOY_PID="$!"
     echo "KICK: started Envoy as PID $ENVOY_PID"
 
